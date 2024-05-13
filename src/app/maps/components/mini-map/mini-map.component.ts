a3c4a103c56cb125007109b8583a2695e3f6a668
Marco Valdez <valdez.rodea.marcoantonio@gmail.com>
2024-05-13 16:24:55 -0600

    houses section complete

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent  implements AfterViewInit{
+ // pasamos la referencia del viewChild
+@ViewChild('map') divMap?: ElementRef;
+
+ //
+@Input() lngLat?:[number,number];
+
+ngAfterViewInit(): void {
+  if (!this.lngLat) throw 'LngLat cant be null';
+  if (!this.divMap) throw 'elemento html no fue encontrado';
+
+  const map = new Map({
+    container: this.divMap.nativeElement, // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: this.lngLat, // starting position [lng, lat]
+    zoom: 10, // starting zoom
+    interactive:false,
+  });
+
+  new Marker({
+    color:'red'
+  })
+  .setLngLat(this.lngLat)
+  .addTo(map)
+}
 
 }
